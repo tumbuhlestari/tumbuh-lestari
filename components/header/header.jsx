786738c1de11d109013e7ceed1500db487fd0296
fhrlzmn <fhrlzmn26@gmail.com>
2024-07-20 23:03:58 +0700

    fix: sticky header

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -7,12 +7,14 @@ import MobileNavigation from './mobile';
 
 export default function Header() {
   return (
-    <header className='sticky container top-0 flex h-20 items-center justify-between gap-4 bg-background px-4 md:px-6'>
-      <div>
-        <Image src={Logo} alt='Tumbuh Lestari' width={192} />
+    <header className='sticky z-50 top-0 bg-background'>
+      <div className='container flex h-20 items-center justify-between gap-4 px-4 md:px-6'>
+        <div>
+          <Image src={Logo} alt='Tumbuh Lestari' width={192} />
+        </div>
+        <NavLinks />
+        <MobileNavigation />
       </div>
-      <NavLinks />
-      <MobileNavigation />
     </header>
   );
 }
