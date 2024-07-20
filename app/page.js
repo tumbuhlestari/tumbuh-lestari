700994f4b16624eb5d8beaf0b317c98d77bc90f4
fhrlzmn <fhrlzmn26@gmail.com>
2024-07-20 23:52:13 +0700

    feat: add footer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Hero from '@/components/home/hero';
 import AboutUs from '@/components/home/about-us';
+import Footer from '@/components/footer';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <AboutUs />
+
+      <Footer />
     </main>
   );
 }
---
+++ components/footer.jsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { links } from './header/links';
+
+import Instagram from '@/public/icons/instagram.svg';
+import Facebook from '@/public/icons/facebook.svg';
+
+export default function Footer() {
+  return (
+    <footer className='bg-foreground text-white'>
+      <div className='mx-auto container  grid grid-cols-2 md:grid-cols-[1fr_1fr_1fr_1.2fr] gap-4 md:gap-8 p-8 '>
+        <div className='space-y-4'>
+          <h3 className='text-xl font-semibold'>Quick</h3>
+          <div className='flex flex-col gap-2'>
+            {links.map((link) => (
+              <Link
+                key={link.id}
+                href={link.path}
+                className='hover:font-semibold'
+              >
+                {link.label}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className='space-y-4'>
+          <h3 className='text-xl font-semibold'>Connect with</h3>
+          <div className='flex flex-col gap-2'>
+            <Link
+              href='mailto:tumbuhlestari01@gmail.com'
+              className='hover:font-semibold'
+            >
+              tumbuhlestari01@gmail.com
+            </Link>
+            <Link href='tel:+6280981823808' className='hover:font-semibold'>
+              +6280981823808
+            </Link>
+          </div>
+        </div>
+        <div className='space-y-4'>
+          <h3 className='text-xl font-semibold'>Kerja Sama</h3>
+          <div className='flex flex-col gap-2'>
+            <p>Gudang Madala Haji</p>
+            <p>Petani Milenial Sukarame</p>
+          </div>
+        </div>
+        <div className='space-y-4'>
+          <h3 className='text-xl font-semibold'>Subscribe now</h3>
+          <div className='flex gap-2'>
+            <Link href='#'>
+              <Image src={Instagram} alt='Instagram' width={24} height={24} />
+            </Link>
+            <Link href='#'>
+              <Image src={Facebook} alt='Facebook' width={24} height={24} />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className='text-center py-4 font-semibold'>
+        <p>Copyright © 2024 Tumbuh Lestari</p>
+      </div>
+    </footer>
+  );
+}
